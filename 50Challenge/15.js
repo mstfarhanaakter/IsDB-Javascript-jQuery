d0e680445ddb84531a947445ff98036b6d6a647a
mstfarhanaakter <farhanashetu.idb@gmail.com>
2025-06-30 12:44:54 +0600

    logic

--- 50Challenge/15.js
+++ 50Challenge/15.js
@@ -2,13 +2,15 @@
 
 function fibonacci(n){
     if (n===0){
-        return 0;
+        return 0; // first fibonacci number
     }
     else if (n === 1){
-        return 1;
+        return 1; // second fibonacci number
     }
     else{
         return fibonacci (n-1) + fibonacci (n-2)
+            // fibonacci (n-1) the previous Fibonacci number
+            //ibonacci(n - 2) → the one before that
     }
 }
 console.log("7th Fibonacci number is:", fibonacci(10));
